22fc4ef4836688ebdedceb1a259246bba8dc0aaa
hhaerin95-ai <hhaerin95@gmail.com>
2026-05-26 21:05:17 +0800

    add images table migration

---
+++ database/migrations/2026_05_26_130426_create_images_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+{
+    Schema::create('images', function (Blueprint $table) {
+        $table->id();
+        $table->unsignedBigInteger('product_id');
+        $table->string('image_path');
+        $table->boolean('is_primary')->default(false);
+        $table->timestamps();
+
+        $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+    });
+}
+};
